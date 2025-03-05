412feb0759f588169a71282d995d8ff6053a51f5
bernikr <git@berni.kr>
2025-03-05 11:51:42 +0100

    Bump version to 0.1.2

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ from paho.mqtt.reasoncodes import ReasonCode
 
 from pypush_gsa_icloud import AppleHeaders
 
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 
 load_dotenv()
 TRUSTED_DEVICE = bool(os.environ.get("TRUSTED_DEVICE"))
